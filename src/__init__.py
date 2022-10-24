786686159500208471b0e149bfa6341e18461620
Kyle King <KyleKing@users.noreply.github.com>
2022-10-24 07:47:11 -0400

    test: filter beartype warning

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,11 @@
 """community-rpg source code."""
 
+from warnings import filterwarnings
+
+from beartype.roar import BeartypeDecorHintPep585DeprecationWarning
+
 __version__ = "0.1.0"
 __pkg_name__ = "community_rpg"
+
+# FYI: https://github.com/beartype/beartype#are-we-on-the-worst-timeline
+filterwarnings("ignore", category=BeartypeDecorHintPep585DeprecationWarning)
